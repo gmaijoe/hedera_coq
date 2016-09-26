b5fa77aa3e5a474bb2b49211ea767d2c3ee924a3
Gavin <gavinmai@stanford.edu>
2016-09-25 17:10:41 -0700

    initial commit for pear hackathon's work

---
+++ Desktop/workspace/Mario_Pear_Project/routes/index.js
@@ -0,0 +1,23 @@
+// Library Requirements
+var express = require('express');
+var router = express.Router();
+var $ = require('jQuery');
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index');
+});
+
+router.get('/filter', function(req, res, next) {
+  res.render('filter');
+});
+
+router.get('/select_frame', function(req, res, next) {
+  res.render('select_frame');
+});
+
+router.get('/share', function(req, res, next) {
+  res.render('share');
+});
+
+module.exports = router;
